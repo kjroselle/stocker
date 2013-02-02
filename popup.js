35acf3f5a5875ca18c8980d1084730aa4b356d5a
Kevin Roselle <kjroselle@gmail.com>
2013-02-01 18:28:32 -0800

    cleaned out some old code

--- popup.js
+++ popup.js
@@ -1,20 +1,23 @@
-
+// URL segments for Yahoo API
 var baseYahooUrl = "http://query.yahooapis.com/v1/public/yql?q="
 var UrlOptions = "&env=store://datatables.org/alltableswithkeys&format=json"
-//http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(%22MSFT%22)&env=store://datatables.org/alltableswithkeys&format=json
-//http://query.yahooapis.com/v1/public/yql?q=select%20symbol,LastTradePriceOnly%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(%22MSFT%22)%20or%20symbol%20in%20(%22GOOG%22)&env=store://datatables.org/alltableswithkeys&format=json
 
-//http://d.yimg.com/autoc.finance.yahoo.com/autoc?query=yahoo&callback=YAHOO.Finance.SymbolSuggest.ssCallback
+// test query for google stock
 googJson = JSON.parse('{"query":{"count":1,"created":"2013-01-28T21:27:37Z","lang":"en-US","results":{"quote":{"symbol":"GOOG","LastTradePriceOnly":"750.73","Name":"Google Inc."}}}}');
 
+// internal array to hold user stock info
+// eventually integrate with localStorage
 var stocksArray = new Array();
+
+// HTML table element where the stocks are displayed
 var stockTableElem;
 
 $(document).ready(function(){
-
+  // hide the add stock div when we start
   $("#addStock").hide();
 });
 
+// build the query to be sent to the Yahoo API to fetch stock info
 function queryBuilder(symbols) {
   var query = "select symbol,Name,LastTradePriceOnly from yahoo.finance.quotes where"
   var numSymbols = symbols.length;
@@ -28,7 +31,7 @@ function queryBuilder(symbols) {
   return query;
 }
 
-// Populates the div for adding a new stock
+// Populates the div for adding a new stock to the stock list
 function addStockDiv(stockJson) {
   $("#addStock").empty();
   
@@ -41,13 +44,11 @@ function addStockDiv(stockJson) {
     return;
   }
   
+  // populate HTML elements with Yahoo response results
   var stockName = stockJson.query.results.quote.Name;
   var stockTicker = stockJson.query.results.quote.symbol;
   var stockPrice = stockJson.query.results.quote.LastTradePriceOnly;
   
-  // var stockNameElement = 
-    // '<div id="nameDiv"><div class="ticker">'+stockTicker+'</div>'+
-    // '<div class="name">'+stockName+'</div>'+'</div>';
   var stockNameElement = document.createElement('div');
   stockNameElement.id = 'nameDiv';
   var tempElement = document.createElement('div');
@@ -59,8 +60,6 @@ function addStockDiv(stockJson) {
   tempElement.innerHTML = stockName;
   stockNameElement.appendChild(tempElement);
   
-  
-  // var stockPriceElement = '<span>'+stockPrice+'</span>';
   var stockPriceElement = document.createElement('span');
   stockPriceElement.innerHTML = stockPrice;
   var buyFieldElement = '<input type="text" class="addIn" \
@@ -69,14 +68,12 @@ function addStockDiv(stockJson) {
     id="selltext" placeholder="Sell Price">';
   var addButtonElement = '<button id="addButton">Add</button>';
   
-  
-  // var addButtonImgElement = '<a href="#" onMouseOver="changeImage()"><img name="addButtonImg" src="res/add.png" width="22" height="22" border="0" alt="javascript button"></a>';
-  
   $("#addStock").append(stockNameElement, stockPriceElement, 
     buyFieldElement, sellFieldElement, addButtonElement);
   
   $("#addStock").show();
   
+  // add handler for button to add the stock to the stock list
   // need to make an anonymous function in order to pass in arguments
   document.querySelector('button#addButton').addEventListener('click', 
     function() {
@@ -89,11 +86,13 @@ function changeImage() {
   return true;
 }
 
-function getSite(queryUrl) {
+// send query to Yahoo and handle response
+function getSiteJson(queryUrl) {
   var request = new XMLHttpRequest();
   request.open("GET", queryUrl, true);
   console.log(queryUrl);
   console.log(request);
+  // asynchronous call handler
   request.onreadystatechange = function () {
     if (request.readyState == 4) {
       if (request.status == 200) {
@@ -108,26 +107,23 @@ function getSite(queryUrl) {
   request.send(null);
 }
 
-function search() {
-  // var text = document.search.searchtext.value;
+function searchButtonHandler() {
   var stockSymbol = [document.getElementById("searchtext").value];
   var query = queryBuilder(stockSymbol);
   console.log(stockSymbol);
   console.log(query);
   var queryUrl = baseYahooUrl+query+UrlOptions;
   console.log(queryUrl);
-  // getSite(queryUrl);
-  addStockDiv(googJson);
+  getSiteJson(queryUrl);
   console.log("done search");
 }
 
 function delStockButtonHandler(elem, row) {
   var rowIndex = row.rowIndex;
+  // update internal stocks array and
+  // HTML table
   stocksArray.splice(rowIndex,1);
   stockTableElem.deleteRow(rowIndex);
-  // rowIndex = elem.parentNode.parentNode.rowIndex;
-  // stockTableElem.deleteRow(rowIndex);
-  // alert('delete ' + rowIndex);
 }
 
 function addStockButtonHandler(name, price) {
@@ -141,17 +137,6 @@ function addStockButtonHandler(name, price) {
   delButtonElement.id = 'delButton';
   delButtonElement.innerHTML = 'Del';
   
-  // var stockDivElement = document.createElement('div');
-  // stockDivElement.className = 'stockEntry';
-  
-  // stockDivElement.appendChild(name);
-  // stockDivElement.appendChild(price);
-  // stockDivElement.appendChild(buyPriceElement);
-  // stockDivElement.appendChild(sellPriceElement);
-  // stockDivElement.appendChild(delButtonElement);
-  
-  // $("#stocks").append(stockDivElement);
-  
   // object to store newly added stock data
   var stockEntry = new Object();
   stockEntry.name = name.firstChild.innerHTML;
@@ -164,6 +149,7 @@ function addStockButtonHandler(name, price) {
   
   console.log(stockEntry.name + " " + stockEntry.ticker+ " " +stockEntry.price);
   
+  // construct HTML table entry
   var rowDelElement = document.createElement('button');
   rowDelElement.innerHTML = "Del";
   
@@ -190,10 +176,9 @@ function addStockButtonHandler(name, price) {
   
 }
 
-// Add event listeners once the DOM has fully loaded by listening for the
-// `DOMContentLoaded` event on the document, and adding your listeners to
-// specific elements when it triggers.
+// listen for when the document is fully loaded then add
+// search button listener and get reference to table element
 document.addEventListener('DOMContentLoaded', function () {
-  document.querySelector('button#searchButton').addEventListener('click', search);
+  document.querySelector('button#searchButton').addEventListener('click', searchButtonHandler);
   stockTableElem = document.getElementById("stockTable");
 });
